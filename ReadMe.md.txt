7d69f14af57a93b79321db51868130cbc6b750a3
Michael Christy <emonomics@gmail.com>
2016-09-16 16:12:30 -0400

    Initial commit for web app

---
+++ ReadMe.md.txt
@@ -0,0 +1,3 @@
+This little ditty is for firefighters or planners who need to use ISO requirements to calculate the required gallons per minute that would be needed to put out a fire at a buildling and is typically part of the preplanning.
+
+I've err'd on the side of caution with some of the coefficeints to keep the app simple but accurate after rounding.
---
+++ scripts/app.js
@@ -0,0 +1,83 @@
+//written by: Michael Christy
+//written for: Louisivlle Fire Department
+//Purpose: Used to calculate required GPM for structure fire planning per ISO.
+//Note: written in just JavaScript to compensate for possible blocking of jquery or frameworks by IT Security
+
+
+var length;
+var width;
+var floors;
+var squareFoot;
+var occupancyFactor;
+var adjustedGPM;
+var exposureDist;
+var final;
+
+//calculates the effective area of the subject building
+function effectiveArea(){
+     if (floors > 1) {
+    //each floor above the first is calculated at 1/2 the area add added to the first.
+    squareFoot = Math.sqrt (((length * width) + (floors - 1) * (0.5 * (length * width))));
+    } else {
+    squareFoot = Math.sqrt(length*width);
+    };
+    return Math.ceil(squareFoot);
+};
+
+//get the coefficent used by the exposures. Only need the max of the options.  
+function exposure(){
+    for (var i = 0; i < document.isoCalc.exposures.length; i++) {
+        if (document.isoCalc.exposures[i].checked) {
+            return exposureDist = document.isoCalc.exposures[i].value;
+        }
+    };
+} 
+
+//takes the coefficients from occupancy and exposures and mutliplies them with the effective area. still requires rounding.
+function maths (occupancyFactor, exposuresDist) {
+    final = squareFoot * occupancyFactor * (1 + exposuresDist);
+    return final;
+};
+
+//iso requires rounding at certain points
+function finalRounding (i) {
+    squareFoot * 18 * i;
+    if (final > 8000) {
+        adjustedGPM = 8000
+    } else if (final < 500) {
+        adjustedGPM = 500
+    } else {
+        adjustedGPM = Math.ceil(final);
+    };
+    return adjustedGPM;
+};
+
+   
+
+//recalculate when user inputs something new then displays on the page after rounding
+ document.onchange = function() {
+    length = document.getElementById("length").value;
+    width = document.getElementById("width").value;
+    floors = document.getElementById("floors").value;
+    effectiveArea();
+    //exposure();
+    for (var i = 0; i < document.isoCalc.combust.length; i++) {
+        if (document.isoCalc.combust[i].checked) {
+            occupancyFactor = document.isoCalc.combust[i].value;
+        }
+    };
+    for (var i = 0; i < document.isoCalc.exposures.length; i++) {
+        if (document.isoCalc.exposures[i].checked) {
+            exposureDist = document.isoCalc.exposures[i].value;
+        }
+    };
+    maths();
+    document.getElementById("class1").value = finalRounding (1.5);
+//document.getElementById("class2").value = finalRounding (1);
+//document.getElementById("class3").value = finalRounding (0.8);
+//document.getElementById("class4").value = finalRounding (0.6);
+ };
+   
+
+
+//min is 6000 for 1story regardless
